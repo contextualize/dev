692f1f45916c32b3a17c85287829769d899dacfc
oscar <oscar@wahltinez.org>
2022-12-15 22:16:55 +1100

    Add demos

--- gen.js
+++ gen.js
@@ -1,19 +1,36 @@
 (function () {
     const self = document.currentScript;
+    // const url = new URL('https://iguanagothere.azurewebsites.net');
+    const url = new URL('http://localhost:7071');
     const gentxt = async () => {
-        const txt = document.createElement('p');
-        txt.innerText = 'Loading...';
-        self.after(txt);
-
-        const prompt = self.dataset['prompt'];
-        const url = new URL('https://iguanagothere.azurewebsites.net/api/opeanai_text_completion');
-        url.searchParams.set('prompt', prompt);
-        const data = await fetch(url).then(res => res.json());
-        txt.innerText = data.result;
+        url.pathname = '/api/openai_text_completion';
+        url.searchParams.set('prompt', self.dataset['prompt']);
+        const data = fetch(url).then(res => res.json()).then(x => x.result);
+        if (self.dataset.callback) {
+            window[self.dataset.callback](await data);
+        } else {
+            const txt = document.createElement('p');
+            txt.innerText = self.dataset.loading || 'Loading...';
+            self.after(txt);
+            txt.innerText = await data;
+        }
     };
 
-    const genimg = () => {
-        // TODO.
+    const genimg = async () => {
+        const size = parseInt(self.dataset.size) || 256;
+        url.pathname = '/api/openai_image_generation';
+        url.searchParams.set('size', size);
+        url.searchParams.set('prompt', self.dataset.prompt);
+        const data = fetch(url).then(res => res.json()).then(x => x.result);
+        if (self.dataset.callback) {
+            window[self.dataset.callback](await data);
+        } else {
+            const img = document.createElement('img');
+            img.width = size;
+            img.src = self.dataset.loading || '';
+            self.after(img);
+            img.src = await data;
+        }
     };
 
     const type = self.dataset['type'];
